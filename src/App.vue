ab22f23c283e122bb257f29c710b1a89048f9372
fto-exerp <fto@exerp.com>
2023-05-17 17:33:51 +0200

    minor fixes to main files

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
-  Hello World
+  <Layout-Top v-if="!auth.check() && route.fullPath !== '/login' && route.fullPath !== '/register'" />
+  <Layout-TopLogged v-if="auth.check()" />
+  <router-view :key="route.fullPath"></router-view>
+  <Layout-footer v-if="!auth.check() && route.fullPath !== '/login' && route.fullPath !== '/register'"/>
 </template>
 <script setup>
+  import { useRoute } from "vue-router/dist/vue-router";
+  import { Auth } from "@/store/auth";
+  import { Global } from "@/store/global";
+
+  const auth = Auth();
+  const route = useRoute();
+  const global = Global();
+
+  if ( auth.check() ) {
+    global.setGlobalData();
+  }
 </script>
